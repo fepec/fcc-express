793cfb95bc85b7597da51f75f1b7f454f7e81049
fepec <felipe.estrada.prada@gmail.com>
2023-11-19 17:45:48 -0500

    Exercise 3

--- myApp.js
+++ myApp.js
@@ -7,8 +7,17 @@ console.log("Hello World")
 // There should be a app.listen(port) command here. 
 // However, because its fcc, that is in server.js
 
-app.get('/', function(req,res) {
-    res.send('Hello Express');
+// 2: Start an express server and serve a string
+
+// app.get('/', function(req,res) {
+//     res.send('Hello Express');
+// })
+
+// 3. Serve an html file.
+let absolutePath = __dirname + '/views/index.html'
+
+app.get('/', function(req, res) {
+    res.sendFile(absolutePath)
 })
 
 
