e59a8c223e9f5b2f76ad3f7cbad9135a1a1f6296
fepec <felipe.estrada.prada@gmail.com>
2023-11-19 17:11:25 -0500

    Exercise 2: serve string

--- myApp.js
+++ myApp.js
@@ -2,8 +2,14 @@ let express = require('express');
 let app = express();
 
 
+console.log("Hello World")
 
+// There should be a app.listen(port) command here. 
+// However, because its fcc, that is in server.js
 
+app.get('/', function(req,res) {
+    res.send('Hello Express');
+})
 
 
 
